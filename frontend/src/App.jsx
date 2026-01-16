35648ce8b25825e351e6bbe43cfcb493cb7df64f
Tushar-Sahu7 <sahutushar532@gmail.com>
2026-01-16 13:09:34 +0530

    chore: scaffold React app and configure Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='m-4 p-4 bg-amber-400 rounded-2xl text-center font-extrabold font-serif'>Hello</div>
+    </>
+  )
+}
+
+export default App
